8db2ec2bdb6b584edd5c6c37651175a5b510a744
Yoriyasu Yano <yorinasub17@gmail.com>
2017-06-04 19:21:34 -0700

    more tests

--- types/E/test.ml
+++ types/E/test.ml
@@ -21,9 +21,39 @@ let test1 test_ctxt =
 ;;
 
 
+let test2 test_ctxt =
+    let example =
+        Let ((Cat ((StrLit "bar"), (StrLit "baz"))), "foo", Plus (Plus (NumLit 5, NumLit 6), Times (NumLit 7, Len (Var "foo"))))
+    in
+    let expectedReducedExample =
+        Plus (Plus (NumLit 5, NumLit 6), Times (NumLit 7, Len (Cat ((StrLit "bar"), (StrLit "baz")))))
+    in
+    assert_equal (reduceLetExpressions example (Hashtbl.create 100)) expectedReducedExample
+;;
+
+
+let test3 test_ctxt =
+    let example =
+        Plus (Plus (NumLit 5, NumLit 6), Times (NumLit 7, Len (Cat ((StrLit "bar"), (StrLit "baz")))))
+    in
+    let expectedTypedExample =
+        (NUM,
+         TypedPlus
+         ((NUM, TypedPlus ((NUM, TypedNumLit 5), (NUM, TypedNumLit 6))),
+          (NUM, TypedTimes ((NUM, TypedNumLit 7),
+                            (NUM, TypedLen (STR,
+                                            TypedCat ((STR, TypedStrLit "bar"),
+                                                      (STR, TypedStrLit "baz"))))))))
+    in
+    assert_equal (assignType example) expectedTypedExample
+;;
+
+
 let suite =
     "suite">:::
-        ["test1">:: test1]
+        ["test1">:: test1;
+         "test2">:: test2;
+         "test3">:: test3]
 ;;
 
 
